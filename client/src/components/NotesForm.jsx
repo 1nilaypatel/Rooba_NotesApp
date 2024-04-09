807298ff7f024cd48028f9094cd44d0eb5be36df
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-04-09 12:40:52 +0530

    _Fix_NotesForm

--- client/src/components/NotesForm.jsx
+++ client/src/components/NotesForm.jsx
@@ -23,56 +23,6 @@ export default function NotesForm({ formData, handleChange, handleSubmit }) {
           value={formData.description}
         />
       </div>
-
-      {/* <div className='flex flex-col gap-5'>
-        <div className="flex flex-col gap-2">
-          <p className='font-semibold'>
-            Images:
-            <span className="font-normal ml-2">
-              Upload images for Book Display (max 2)
-            </span>
-          </p>
-          <div className="flex gap-2 bg-slate-50 rounded-lg mt-3">
-            <input
-              onChange={(e) => setFiles(e.target.files)}
-              type="file" 
-              id="images"
-              accept="image/*"
-              className="p-2 rounded-lg w-full"
-              multiple
-            />
-            <button
-              onClick={handleImageSubmit}
-              type='button'
-              className="p-2 text-indigo-400 border border-indigo-400 rounded-lg uppercase hover:shadow-lg disabled:bg-opacity-40"
-            >
-              {uploading ? 'Uploading...' : 'Upload'}
-            </button>
-          </div>
-        </div>
-        <p className='text-red-500 text-sm'>
-          {imageUploadError}
-        </p>
-        {formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
-          <div
-            key={url}
-            className='flex justify-between p-2 border border-indigo-400 items-center '
-          >
-            <img
-              src={url}
-              alt='listing image'
-              className='w-20 h-20 object-contain rounded-lg'
-            />
-            <button
-              type='button'
-              onClick={() => handleRemoveImage(index)}
-              className='p-3 text-red-500 rounded-lg uppercase hover:opacity-75'
-            >
-              Delete
-            </button>
-          </div>
-        ))}
-      </div> */}
     </form>
   )
 }
